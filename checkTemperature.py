82c61a593f98016bde53b693092a5853d4d2680a
Khan <hellotendxd@gmail.com>
2023-11-27 12:38:43 +0400

    Create my project with my own module for temperature

---
+++ checkTemperature.py
@@ -0,0 +1,4 @@
+import myTemperature as a
+
+print("The f of 32 is", a.mytemperaturectof(32))
+print("The c of 31 is", a.mytemperatureftoc(31))
---
+++ myTemperature.py
@@ -0,0 +1,6 @@
+def mytemperaturectof(c):
+                f = 9/5*c + 32
+                return f
+def mytemperatureftoc(f):
+                c = 5/9*f - 32
+                return c
